509a5d6c5bf83e07ef279b148f571476979bbba1
Shogo Kitada <contact@shogo0x2e.com>
2024-03-05 17:18:40 +0900

    console.log を追加

--- src/app/api/shutter/route.ts
+++ src/app/api/shutter/route.ts
@@ -1,6 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = () => {
+export const GET = (req: NextRequest) => {
+  console.log(`[Shutter] Shutter was pressed ${18.556}`);
+
   return NextResponse.json(
     {
       message: `Shutter was pressed ${18.556}`,
