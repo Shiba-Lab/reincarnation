2cedf4c421e04104edb6ec1ed73e6839a3defe36
Shogo Kitada <contact@shogo0x2e.com>
2024-03-09 21:57:02 +0900

    socket 作成

--- src/app/ws-test/page.tsx
+++ src/app/ws-test/page.tsx
@@ -1,35 +1,73 @@
 "use client";
 
-//パスは変更してください
-import { useEffect, useState } from "react";
-import { ServerToClientEvents, ClientToServerEvents } from "../../model/model";
-import { io, Socket } from "socket.io-client";
 import { Button } from "@/components/ui/button";
+import { socket } from "@/lib/socket";
+import { useEffect, useState } from "react";
 
-export default function Home() {
-  // FIXME: Production では、サーバーのURLを環境変数から取得するようにする
-  const [socket, setSocket] = useState<
-    Socket<ServerToClientEvents, ClientToServerEvents>
-  >(io("http://localhost:3001"));
+const Page = () => {
+  const [value, setValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
+  const [messages, setMessages] = useState<string[]>([]);
 
   useEffect(() => {
-    socket.on("connect", () => {
-      console.log(socket.connected);
-    });
+    const onConnect = () => {
+      setIsConnected(true);
+    };
 
-    socket.on("hello", (message) => {
-      console.log(message);
-    });
+    const onDisconnect = () => {
+      setIsConnected(false);
+    };
+
+    const onHelloEvent = (value: string) => {
+      setMessages((prev) => [...prev, value]);
+      setIsLoading(false);
+      setValue("");
+    };
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("hello", onHelloEvent);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("hello", onHelloEvent);
+    };
   }, []);
 
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    console.log("emit", value);
+
+    socket.timeout(5000).emit("message", value);
+  };
+
   return (
-    <Button
-      variant="secondary"
-      onClick={() => {
-        socket.emit("message", "hello world");
-      }}
-    >
-      Send "hello world"
-    </Button>
+    <div>
+      <p>State: {"" + isConnected}</p>
+      <h1>Messages</h1>
+      <ul>
+        {messages.map((message, i) => (
+          <li key={i}>{message}</li>
+        ))}
+      </ul>
+      <Button onClick={() => socket.connect()}>Connect</Button>
+      <Button onClick={() => socket.disconnect()}>Disconnect</Button>
+      <div className="w-full bg-gray-500">divider</div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
+        <Button type="submit" disabled={isLoading}>
+          Send
+        </Button>
+      </form>
+    </div>
   );
-}
+};
+
+export default Page;
